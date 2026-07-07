7470a9af4a284c1adac03aad4702d54ca0f2a596
João Guedes <joaopedrochg@gmail.com>
2026-07-06 23:36:50 -0300

    Server framework 101, chapters 1, 2, 3, 4, 5 e 6

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Real Estate",
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        #Security
+        'security/ir.model.access.csv',
+            
+        #Views
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        ],  
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,35 @@
+from odoo import fields, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Properties for the Real Estate Model'
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, 
+                                    default= lambda self: datetime.today() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+            string='Garden Orientation',
+            selection=[('north', 'North'), ('west', 'West'), ('south', 'South'), ('east', 'East')],
+            help="Garden Orientation is used to indicate the orientation of the property's garden.")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+            string='State',
+            selection=[('new', 'New'), ('offer received', 'Offer Received'),
+                       ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'),
+                       ('cancelled', 'Cancelled')],
+            help="Possible states for an indexed property.",
+            required=True,
+            default='new')
